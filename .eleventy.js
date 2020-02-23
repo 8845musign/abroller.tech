632ee72fe38e9cca7f5e090093ef4130ff0c7517
8845musign <8845musign@gmail.com>
2020-02-23 22:04:02 +0900

    change date format to jp

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
+const dayjs = require('dayjs')
+require('dayjs/locale/ja')
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('src/assets');
+
   eleventyConfig.addPassthroughCopy('src/_redirects');
+
+  eleventyConfig.addNunjucksFilter("dateJp", (value) => {
+    return dayjs(value).format('YYYY/MM/DD')
+  });
 
   return {
     dir: {
